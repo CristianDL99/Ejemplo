63e2534a54db5758073746811bbd4c833994dbe2
Cristhian Desposorio Loyola <N00074932@upn.pe>
2020-10-01 11:35:53 -0500

    Añadi boton y titulo

--- Proyecto Moanso_Ejemplo/Form1.Designer.cs
+++ Proyecto Moanso_Ejemplo/Form1.Designer.cs
@@ -28,7 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnEmpezar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // btnEmpezar
+            // 
+            this.btnEmpezar.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnEmpezar.Font = new System.Drawing.Font("Myanmar Text", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEmpezar.ForeColor = System.Drawing.Color.Maroon;
+            this.btnEmpezar.Location = new System.Drawing.Point(261, 216);
+            this.btnEmpezar.Name = "btnEmpezar";
+            this.btnEmpezar.Size = new System.Drawing.Size(118, 32);
+            this.btnEmpezar.TabIndex = 0;
+            this.btnEmpezar.Text = "Empezar";
+            this.btnEmpezar.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Mutant Academy DC BB", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(166, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(315, 38);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Proyecto MOANSO";
             // 
             // Form1
             // 
@@ -36,14 +61,20 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::Proyecto_Moanso_Ejemplo.Properties.Resources.fondo_elegante_textura_blanca_23_2148438405;
             this.ClientSize = new System.Drawing.Size(631, 326);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnEmpezar);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnEmpezar;
+        private System.Windows.Forms.Label label1;
     }
 }
 
