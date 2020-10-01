6cd8ab657e03a879f9ea24a4ca5c79625ee39040
Ethel Marin <N00105378@upn.pe>
2020-10-01 12:01:09 -0500

    agregue picture box

--- Proyecto Moanso_Ejemplo/Form1.Designer.cs
+++ Proyecto Moanso_Ejemplo/Form1.Designer.cs
@@ -28,8 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.btnEmpezar = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // btnEmpezar
@@ -37,9 +40,10 @@
             this.btnEmpezar.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.btnEmpezar.Font = new System.Drawing.Font("Myanmar Text", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnEmpezar.ForeColor = System.Drawing.Color.Maroon;
-            this.btnEmpezar.Location = new System.Drawing.Point(261, 216);
+            this.btnEmpezar.Location = new System.Drawing.Point(348, 266);
+            this.btnEmpezar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnEmpezar.Name = "btnEmpezar";
-            this.btnEmpezar.Size = new System.Drawing.Size(118, 32);
+            this.btnEmpezar.Size = new System.Drawing.Size(157, 39);
             this.btnEmpezar.TabIndex = 0;
             this.btnEmpezar.Text = "Empezar";
             this.btnEmpezar.UseVisualStyleBackColor = false;
@@ -48,24 +52,38 @@
             // 
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Font = new System.Drawing.Font("Mutant Academy DC BB", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(166, 60);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(221, 74);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(315, 38);
+            this.label1.Size = new System.Drawing.Size(445, 54);
             this.label1.TabIndex = 1;
             this.label1.Text = "Proyecto MOANSO";
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(619, 186);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(143, 131);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::Proyecto_Moanso_Ejemplo.Properties.Resources.fondo_elegante_textura_blanca_23_2148438405;
-            this.ClientSize = new System.Drawing.Size(631, 326);
+            this.ClientSize = new System.Drawing.Size(841, 401);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnEmpezar);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -75,6 +93,7 @@
 
         private System.Windows.Forms.Button btnEmpezar;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
